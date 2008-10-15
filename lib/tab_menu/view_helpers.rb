8b078195a3fc7f6c831d4c689be94a1a0efb3111
Dan Pickett <dpickett@enlightsolutions.com>
2008-10-15 19:42:38 -0400

    need to test against erb implementation

--- lib/tab_menu/view_helpers.rb
+++ lib/tab_menu/view_helpers.rb
@@ -7,12 +7,16 @@ module TabMenu
     
     def tab_menu(tab_name = "", html_options = {}, &block)
       html_options[:class] ||= tab_name.blank? ? nil : tab_name.gsub(" ", "_").underscore
+      html_option_strings = []
+      html_options.each do |key, value|
+        html_option_strings << "#{key}=\"#{value}\""
+      end
       
       # unsure of performance here but nested content_tags were causing issues refer to - 
       # refer to - http://rails.lighthouseapp.com/projects/8994/tickets/386-nested-content_tag-calls-causes-the-layout-to-render-multiple-times
       # will fix in 2.2 after double render gets fixed
       if eval("defined? _erbout", block.binding)
-        concat("<ul>", block.binding)
+        concat("<ul #{html_option_strings.join}>", block.binding)
         yield(TabBuilder.new(self))
         concat("</ul>", block.binding)
       else
--- spec/tab_menu/view_helpers_spec.rb
+++ spec/tab_menu/view_helpers_spec.rb
@@ -2,11 +2,39 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 require File.dirname(__FILE__) + "/../../lib/tab_menu"
 
-describe TabMenu::ViewHelpers, :type => :helper do
+describe TabMenu::ViewHelpers, "helper", :type => :helper do
   include ActionView::Helpers::UrlHelper
   include ActionView::Helpers::TextHelper
   include ActionView::Helpers::TagHelper
   include TabMenu::ViewHelpers
+  
+  it "should create an unordered list" do
+    menu = helper.tab_menu do |t|
+      t.tab "Test", "/"
+    end
+    
+    menu.should =~ /^<ul>/
+    menu.should =~ /<\/ul>$/
+  end
+  
+  it "should optionally take a name which will create a subsequent class for the ul" do
+    menu = tab_menu("test") do |t|
+      t.tab "Test", "/"
+    end
+    
+    menu.should =~ /<ul class=\"test\">/
+  end
+end
+
+describe TabMenu::ViewHelpers, "erb", :type => :helper do
+  include ActionView::Helpers::UrlHelper
+  include ActionView::Helpers::TextHelper
+  include ActionView::Helpers::TagHelper
+  include TabMenu::ViewHelpers
+  
+  before(:each) do
+    pending("must figure out how to mock definition of erbout - is this worth it with defect fix pending?")
+  end
   
   it "should create an unordered list" do
     menu = helper.tab_menu do |t|
