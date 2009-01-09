a72e8c161978216739bfa152d755fdc42b5fa1df
Dan Pickett <dpickett@enlightsolutions.com>
2009-01-09 18:14:17 -0500

    adding recent changes that add ability to set the current tab

--- lib/tab_menu/tab_builder.rb
+++ lib/tab_menu/tab_builder.rb
@@ -3,21 +3,27 @@ module TabMenu
     include ActionView::Helpers::TagHelper
     include ActionView::Helpers::CaptureHelper
     
-    def initialize(controller)
+    def initialize(controller, current_tab = nil)
       @controller = controller
+      @current_tab = current_tab
     end
     
     def tab(name, url, html_options = {})
       #append current to the contentli class attribute if its the current page
-      html_options[:class] = [html_options[:class], "current"].join if @controller.current_page?(url)
+      if (!@current_tab.nil? && @current_tab.downcase == name.downcase) || (@current_tab.nil? && @controller.current_page?(url))
+        html_options[:class] = [html_options[:class], "current"].join(" ").strip()
+      end
+      # content_tag(:li, name, html_options)
+      # "<li>" + @controller.link_to(name, url) + "</li>"
       content_tag(:li, name, html_options) do
         @controller.link_to name, url
       end
     end
-    
+
     # hack for rails 2.2.2
     def with_output_buffer(lines=[], &block)
       block.call
     end
+    
   end
 end
--- lib/tab_menu/view_helpers.rb
+++ lib/tab_menu/view_helpers.rb
@@ -5,16 +5,16 @@ module TabMenu
     include ActionView::Helpers::TagHelper
     include ActionView::Helpers::UrlHelper
     
-    def tab_menu(tab_name = "", html_options = {}, &block)
+    def tab_menu(tab_name = "", html_options = {}, options = {}, &block)
       html_options[:class] ||= tab_name.blank? ? nil : tab_name.gsub(" ", "_").underscore
       html_option_strings = []
       html_options.each do |key, value|
         html_option_strings << "#{key}=\"#{value}\""
       end
 
-      concat("<ul #{html_option_strings.join}>", block.binding)
-      yield(TabBuilder.new(self))
-      concat("</ul>", block.binding)
+      concat("<ul #{html_option_strings.join}>")
+      yield(TabBuilder.new(self, options[:current_tab]))
+      concat("</ul>")
     end
     
   end
